9121cf3561444e47a70d08ba049343afc8ea39ba
cvudumbarainformatika <cvudumbarainformatika@gmail.com>
2025-05-24 13:14:29 +0700

    wip: penjualan

--- src/views/admin/transaksi/penjualan/SalesQuickForm.vue
+++ src/views/admin/transaksi/penjualan/SalesQuickForm.vue
@@ -2,51 +2,7 @@
   <div class="flex flex-col h-full space-y-4 md:flex-row gap-4">
     <div class="w-full md:w-1/3 rounded shadow space-y-4">
         <!-- Supplier Selection ini ganti dengan customer nanti-->
-        <div class="bg-white dark:bg-gray-800 rounded-xl shadow-sm p-4 mt-4">
-            <h3 class="text-lg font-medium text-gray-900 dark:text-white mb-4 flex items-center">
-              <Icon name="User" class="w-5 h-5 mr-2 text-primary-500" />
-              Pelanggan
-            </h3>
-              <label for="supplier-search" class="block text-sm font-medium text-gray-700 dark:text-gray-300 mb-1">
-                Supplier <span class="text-red-500">*</span>
-              </label>
-              <div class="relative w-full">
-                <SearchDropdown
-                  id="supplier-search"
-                  v-model="customersearch"
-                  placeholder="Cari supplier..."
-                  :debounce="300"
-                  :min-search-length="3"
-                  item-key="id"
-                  item-label="name"
-                  not-found-text="Supplier tidak ditemukan"
-                  not-found-subtext="Coba kata kunci lain atau tambahkan supplier baru"
-                  add-button-text="Tambah Supplier Baru"
-                  api-url="/api/v1/suppliers"
-                  api-response-path="data.data"
-                  :use-api="true"
-                  @select="handleSupplierSelect"
-                  @add-new="openAddSupplierDialog"
-                  @items-loaded="onSuppliersLoaded"
-                >
-                  <template #item="{ item }">
-                    <div class="font-medium text-secondary-900 dark:text-white">{{ item.name }}</div>
-                    <div class="text-sm text-secondary-500 dark:text-secondary-400 flex items-center gap-2">
-                      <span>{{ item.phone || 'Tidak ada telepon' }}</span>
-                    </div>
-                  </template>
-                </SearchDropdown>
-              </div>
-            </div>
-
-
-            <!-- informasi kasir saat ini    -->
-            <div class="bg-white dark:bg-gray-800 rounded-xl shadow-sm p-4">
-
-              Informasi Data Kasir saat ini
-              dan
-              <div>informasi Info Daftar Product atau informasi apa saja saat ini</div>
-            </div>
+        <CustomerAndKasirSection/>
     </div>
      <!-- Grid Produk -->
      <div class="w-full md:w-2/3 rounded shadow space-y-4">
@@ -61,9 +17,9 @@
 
 <script setup>
 import { ref, defineAsyncComponent } from 'vue';
-const customersearch = ref('');
 
 const ProductListSection = defineAsyncComponent(()=> import('./compQuickForm/ProductListSection.vue'));
+const CustomerAndKasirSection = defineAsyncComponent(()=> import('./compQuickForm/CustomerAndKasirSection.vue'));
 
 </script>
 
---
+++ src/views/admin/transaksi/penjualan/compQuickForm/CustomerAndKasirSection.vue
@@ -0,0 +1,51 @@
+<template>
+  <div class="bg-white dark:bg-gray-800 rounded-xl shadow-sm p-4 mt-4">
+    <h3 class="text-lg font-medium text-gray-900 dark:text-white mb-4 flex items-center">
+      <Icon name="User" class="w-5 h-5 mr-2 text-primary-500" />
+      Pelanggan
+    </h3>
+      <!-- <label for="supplier-search" class="block text-sm font-medium text-gray-700 dark:text-gray-300 mb-1">
+        Pelanggan <span class="text-red-500">*</span>
+      </label> -->
+      <div class="relative w-full">
+        <SearchDropdown
+          id="customer-search"
+          v-model="customersearch"
+          placeholder="Cari Customer..."
+          :debounce="300"
+          :min-search-length="3"
+          item-key="id"
+          item-label="name"
+          not-found-text="Customer tidak ditemukan"
+          not-found-subtext="Coba kata kunci lain atau tambahkan Customer baru"
+          add-button-text="Tambah Customer Baru"
+          api-url="/api/v1/customers"
+          api-response-path="data.data"
+          :use-api="true"
+          @select="handleCustomerSelect"
+          @add-new="openAddCustomerDialog"
+          @items-loaded="onCustomerLoaded"
+        >
+          <template #item="{ item }">
+            <div class="font-medium text-secondary-900 dark:text-white">{{ item.name }}</div>
+            <div class="text-sm text-secondary-500 dark:text-secondary-400 flex items-center gap-2">
+              <span>{{ item.phone || 'Tidak ada telepon' }}</span>
+            </div>
+          </template>
+        </SearchDropdown>
+      </div>
+  </div>
+
+
+  <!-- informasi kasir saat ini    -->
+  <div class="bg-white dark:bg-gray-800 rounded-xl shadow-sm p-4">
+
+    Informasi Data Kasir saat ini
+    dan
+    <div>informasi Info Daftar Product atau informasi apa saja saat ini</div>
+  </div>
+</template>
+
+<script setup>
+
+</script>
