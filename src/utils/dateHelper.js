6143e3fb3f30c524fbefb5b60cb41848624242e3
cvudumbarainformatika <cvudumbarainformatika@gmail.com>
2025-07-13 21:38:03 +0700

    rev:deploy

--- src/utils/dateHelper.js
+++ src/utils/dateHelper.js
@@ -59,3 +59,16 @@ export function getMonthEndDate() {
   const now = new Date()
   return toLocalDateString(new Date(now.getFullYear(), now.getMonth() + 1, 0))
 }
+
+export function formatWaktuLalu(dateInput) {
+  const now = new Date()
+  const date = new Date(dateInput)
+  const diff = (now - date) / 1000 // dalam detik
+
+  if (diff < 60) return `${Math.floor(diff)} detik yang lalu`
+  if (diff < 3600) return `${Math.floor(diff / 60)} menit yang lalu`
+  if (diff < 86400) return `${Math.floor(diff / 3600)} jam yang lalu`
+  if (diff < 2592000) return `${Math.floor(diff / 86400)} hari yang lalu`
+  if (diff < 31104000) return `${Math.floor(diff / 2592000)} bulan yang lalu`
+  return `${Math.floor(diff / 31104000)} tahun yang lalu`
+}
