3c732c92599a003b43e2ab4ab722e89e073a7b71
cvudumbarainformatika <cvudumbarainformatika@gmail.com>
2025-07-20 19:04:52 +0700

    rev: hoaaaam

--- src/services/api.js
+++ src/services/api.js
@@ -3,7 +3,7 @@ import { useAuthStore } from '@/stores/auth'
 
 // Create axios instance
 const api = axios.create({
-  baseURL: import.meta.env.VITE_API_URL || 'http://yaumi.local:8182',
+  baseURL: import.meta.env.VITE_API_URL || 'http://yaumi.local:8182' || 'https://api.yaumi.my.id',
   headers: {
     'Content-Type': 'application/json',
     'Accept': 'application/json'
